46b07d919cb79b901ece390b290b806e82dcb9fc
UBlessing <62104661+UBlessing@users.noreply.github.com>
2020-07-01 21:58:52 +0900

    Update pckt_header.h

--- pckt_header.h
+++ pckt_header.h
@@ -1,9 +1,19 @@
-#define mac_addr_size 6
+struct ether_addr {
+	unsigned char addr[6];
+}__attribute__((packed));
+typedef struct ether_addr ETHER_ADDR;
+
+//사용 안함
+struct ip_addr {
+	unsigned int addr;
+}__attribute__((packed));
+typedef struct ip_addr IP_ADDR;
+//////
 
 struct ether_header {
-	unsigned char ether_dst_mac[mac_addr_size];
-	unsigned char ether_src_mac[mac_addr_size];
-	unsigned short ether_type;
+	ETHER_ADDR dst;
+	ETHER_ADDR src;
+	unsigned short type;
 	/*
 	 * ether types:
 	 * 	0x0800 is IP
@@ -19,18 +29,19 @@ struct ether_header {
 	 * 	0x8863 is PPPoE Discovery Stage
 	 * 	0x8864 is PPPoE PPP Session Stage
 	 */
-};
+}__attribute__((packed));
+typedef struct ether_header ETHER_HEADER;
 
 struct arp_header {
-	unsigned short hardware_type;
-	unsigned short protocol_type;
-	unsigned char hard_addr_len;
-	unsigned char proto_addr_len;
-	unsigned short operation_code;
-	unsigned char src_mac_addr[mac_addr_size];
-	unsigned long src_addr;
-	unsigned char dst_mac_addr[mac_addr_size];
-	unsigned long dst_addr;
+	unsigned short hardw_type; //hardware type
+	unsigned short proto_type; //porotocol type
+	unsigned char hard_addr_len; //hardware address len
+	unsigned char proto_addr_len; //protocal address len
+	unsigned short opcode; //operation code
+	ETHER_ADDR src_mac_addr;
+	unsigned int src_addr;
+	ETHER_ADDR dst_mac_addr;
+	unsigned int dst_addr;
 	/* protocol_type same ether_types.
 	 * if mac addr is "ff:ff:ff:ff:ff:ff", it is broadcast.
 	 * operation code:
@@ -39,4 +50,11 @@ struct arp_header {
 	 * 	3 is Rarp Request
 	 * 	4 is Rarp Reply
 	 */
-};
+}__attribute__((packed));
+typedef struct arp_header ARP_HEADER;
+
+struct arp_packet {
+	ETHER_HEADER ether_hdr;
+	ARP_HEADER arp_hdr;
+}__attribute__((packed));
+typedef struct arp_packet ARP_PACKET;
